9e805160f4f134a112b658785522046fa3cc941c
Sean Altemose <sean@seanaltemose.com>
2019-08-12 22:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const searchTerms = $(`#searchTerms`).val()
+  $.ajax({
+    type: 'GET',
+    url: `https://api.github.com/search/repositories?q=${searchTerms}`,
+    success: function(data) {
+      const $data = $(`data`);
+      console.log('success', data);
+      displayResults(data);
+    }
+  });
+}
